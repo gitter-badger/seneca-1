15a5f23105a58e3d78e01e950073320471c7caec
Lamden Calrissian <lamdencalrissian@users.noreply.github.com>
2018-07-12 16:31:21 -0700

    WiP

---
+++ seneca/smart_contract_user_libs/storage/kv2.py
@@ -0,0 +1,16 @@
+'''
+# Features:
+* KV flat store with one implicit namespace
+* JSON serialization for complex data types
+
+
+'''
+import json
+
+
+def get_k(k, r_value_if_not_found=None):
+    pass
+
+
+def set_k(k, v):
+    pass
